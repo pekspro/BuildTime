1d21ce8031aec1b41888b1218f380ab9080160fe
pekspro <msn@pekspro.com>
2022-07-26 23:09:46 +0200

    Changes print order.

--- BuildTime/Program.cs
+++ BuildTime/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine($"Build time: {BuildTimeProvider.BuildTime}");
 Console.WriteLine($"Build time string: {BuildTimeProvider.BuildTimeString}");
+Console.WriteLine($"Build time: {BuildTimeProvider.BuildTime}");
