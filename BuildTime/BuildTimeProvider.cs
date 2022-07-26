6f0a7ed490288e5dc47bb816c4b5ea28b69ec632
pekspro <msn@pekspro.com>
2022-07-26 19:17:26 +0200

    Adds BuildTime project.

---
+++ BuildTime/BuildTimeProvider.cs
@@ -0,0 +1,14 @@
+﻿public static partial class BuildTimeProvider
+{
+    static BuildTimeProvider()
+    {
+        Initialize();
+    }
+
+    static partial void Initialize();
+
+    public static string? BuildTimeString { get; private set; }
+
+    public static DateTime BuildTime => 
+        BuildTimeString is null ? DateTime.MinValue : DateTime.Parse(BuildTimeString);
+}
